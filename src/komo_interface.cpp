64a473644e617963c65ffefd57602734481596bd
Martin Griesser <martin@martin-mini-book.(none)>
2014-09-09 16:49:52 +0200

    Ready for basic planning requests.

--- src/komo_interface.cpp
+++ src/komo_interface.cpp
@@ -1,1 +1,153 @@
+/**
+ *
+ *
+ *
+ *
+ **/
+#include <komo_interface.h>
+
+
+namespace iis_komo {
+
+KomoInterface::KomoInterface(const string &config_name)
+{
+    _world = new ors::KinematicWorld(config_name.c_str());
+    // used in example - don't know if really necessary...
+    makeConvexHulls(_world->shapes);
+    // enable collision checking for all shapes
+    for(ors::Shape *s:_world->shapes) {
+        s->cont = true;
+    }
+}
+
+KomoInterface::~KomoInterface() {
+    if(_world) {
+        delete _world;
+    }
+}
+
+bool KomoInterface::plan(const vector<double> &start_state, double x, double y, double z, arr &traj)
+{
+    // set initial position
+    arr init_pos(&start_state[0], 7);
+    _world->setJointState(init_pos);
+    // set target position
+    ors::Shape *target = _world->getShapeByName("target");
+    ors::Vector targetPos(x, y, z);
+    target->rel.pos = targetPos;
+
+    ors::Shape *eef = _world->getShapeByName("endeff");
+
+    // make planning request, using only position constraints.
+    return planTo(*_world, *eef, *target, traj);
+}
+
+
+bool KomoInterface::plan(const vector<double> &start_state, double x, double y, double z, double roll, double pitch, double yaw, arr &traj)
+{
+    // set initial position
+    arr init_pos(&start_state[0], 7);
+    _world->setJointState(init_pos);
+
+    ors::Shape *target = _world->getShapeByName("target");
+
+    // set target position...
+    ors::Vector target_pos(x, y, z);
+    target->rel.pos = target_pos;
+
+    // ...and orientation
+    ors::Quaternion q;
+    q.setRpy(roll, pitch, yaw);
+    target->rel.rot = q;
+
+    ors::Shape *eef = _world->getShapeByName("endeff");
+
+    // make planning request and align all axes
+    return planTo(*_world, *eef, *target, traj, 7);
+
+}
+
+bool KomoInterface::planTo(ors::KinematicWorld &world,
+                           ors::Shape &endeff,
+                           ors::Shape &target,
+                           arr &traj,
+                           byte whichAxesToAlign,
+                           uint iterate)
+{
+    //-- parameters
+    double posPrec = MT::getParameter<double>("KOMO/moveTo/precision", 1e4); // original 1e3
+    double colPrec = MT::getParameter<double>("KOMO/moveTo/collisionPrecision", -1e0);
+    double margin = MT::getParameter<double>("KOMO/moveTo/collisionMargin", .1);
+    double zeroVelPrec = MT::getParameter<double>("KOMO/moveTo/finalVelocityZeroPrecision", 1e1);
+    double alignPrec = MT::getParameter<double>("KOMO/moveTo/alignPrecision", 1e4); // original 1e3
+
+    //-- set up the MotionProblem
+    target.cont = false;
+
+    MotionProblem MP(world);
+    MP.loadTransitionParameters();
+    world.swift().initActivations(world);
+
+    TaskCost *c;
+    c = MP.addTask("endeff_pos", new DefaultTaskMap(posTMT, endeff.index, NoVector, target.index, NoVector));
+    MP.setInterpolatingCosts(c, MotionProblem::finalOnly, {0.,0.,0.}, posPrec);
+
+    c = MP.addTask("endeff_vel", new DefaultTaskMap(posTMT, world, "endeff")); //endeff.index));
+    //  c = MP.addTask("q_vel", new DefaultTaskMap(qItselfTMT, world));
+
+    MP.setInterpolatingCosts(c, MotionProblem::finalOnly, NoArr, zeroVelPrec);
+    c->map.order = 1; //make this a velocity variable!
+
+    if(colPrec < 0) { //interpreted as hard constraint
+        c = MP.addTask("collisionConstraints", new CollisionConstraint(margin));
+    } else { //cost term
+        c = MP.addTask("collision", new ProxyTaskMap(allPTMT, {0}, margin));
+    }
+    MP.setInterpolatingCosts(c, MotionProblem::constant, {0.}, colPrec);
+
+    for(uint i=0;i<3;i++) if(whichAxesToAlign&(1<<i)){
+        ors::Vector axis;
+        axis.setZero();
+        axis(i)=1.;
+        c = MP.addTask(STRING("endeff_align_" << i), new DefaultTaskMap(vecAlignTMT, endeff.index, axis, target.index, axis));
+        MP.setInterpolatingCosts(c, MotionProblem::finalOnly, {1.}, alignPrec);
+    }
+
+    //-- create the Optimization problem (of type kOrderMarkov)
+    MotionProblemFunction MF(MP);
+    arr x(MP.T + 1, MP.dim_x());
+    for(uint t=0; t <= MP.T; t++) {
+        x[t]() = MP.x0;  //initialize trajectory
+    }
+
+    //-- optimize
+    for(uint k = 0; k < iterate; k++){
+        MT::timerStart();
+        if(colPrec < 0){
+            optConstrained(x, NoArr, Convert(MF), OPT(verbose=1, stopIters=100, maxStep=.5, stepInc=2., nonStrictSteps=(!k?15:5)));
+        } else {
+            optNewton(x, Convert(MF), OPT(verbose=2, stopIters=100, maxStep=.5, stepInc=2., nonStrictSteps=(!k?15:5)));
+        }
+        cout <<"** optimization time=" << MT::timerRead() <<endl;
+        //    checkJacobian(Convert(MF), x, 1e-5);
+        MP.costReport(false);
+    }
+
+    arr final_state = x[x.d0-1];
+    world.setJointState(final_state);
+
+    cout << "Last point: " << final_state << endl;
+    cout << "EEF final pos: " << endeff.X.pos << endl;
+    cout << "EEF final rot: " << endeff.X.rot << endl;
+
+    traj = x;
+
+//    world.watch(true);
+    displayTrajectory(x, 1, world, "trajectory", 0.1);
+
+    return true;
+}
+
+
+}
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,41 +3,20 @@
 #include <std_msgs/Float64MultiArray.h>
 #include <sensor_msgs/JointState.h>
 #include <ros/ros.h>
+#include <komo_interface.h>
+
+
+#define JOINT_STATES_TOPIC "/simulation/right_arm/joint_control/get_state"
+#define JOINT_MOVE_TOPIC "/simulation/right_arm/joint_control/move"
+
 
 using namespace ros;
 using namespace std;
 
-arr plan(const vector<double> &start_state) {
+ros::Publisher _pub_move;
 
-    ors::KinematicWorld G("kuka.kvg");
-    ors::Shape *t = G.getShapeByName("target");
-  //  makeConvexHulls(G.shapes);
-
-    for(ors::Shape *s:G.shapes) s->cont=true;
-
-    // set initial position and display
-    arr init_pos(&start_state[0], 7);
-
-    G.setJointState(init_pos);
-    ors::Shape *eef = G.getShapeByName("endeff");
-
-    arr x = moveTo(G, *eef, *t, 7);
-    return x;
-
-  //  s->rel.addRelativeTranslation(-0.2,0.2,-0.2);
-  //  s->rel.addRelativeRotationDeg(90,0,0,1);
-  //  x = moveTo(G, *G.getShapeByName("endeff"), *s, 7);
-  //  for(uint i=0;i<1;i++) displayTrajectory(x, 1, G, "planned trajectory", 0.01);
-
-  //  s->rel.addRelativeRotationDeg(90,1,0,0);
-  //  x = moveTo(G, *G.getShapeByName("endeff"), *s, 7);
-  //  for(uint i=0;i<1;i++) displayTrajectory(x, 1, G, "planned trajectory", 0.01);
-}
-
-bool execute(NodeHandle &nh, arr &trajectory) {
-
-    Publisher move_pub = nh.advertise<std_msgs::Float64MultiArray>("/simulation/right_arm/joint_control/move", 1, false);
-
+void executeTrajectory(arr trajectory)
+{
     std_msgs::Float64MultiArray msg;
     msg.layout.dim.resize(1);
     msg.layout.dim[0].size = 7;
@@ -48,38 +27,108 @@ bool execute(NodeHandle &nh, arr &trajectory) {
 
     int size = trajectory.d0 - 1;
 
+    // iterate over trajectory points...
     for (int i = 1; i < size; ++i) {
         arr current_state = trajectory[i-1];
         arr next_state = trajectory[i];
         arr diff = next_state - current_state;
+        // ... calculate interpolation step based on loop rate ...
         arr step = diff / 10.0;
-
+        // ... and move subsequently towards next position.
         for (int j = 0; j < 10; ++j) {
             current_state += step;
             for(int k = 0; k < 7; ++k) {
                 msg.data[k] = current_state(k);
             }
-            move_pub.publish(msg);
+            _pub_move.publish(msg);
             loop_rate.sleep();
         }
     }
 }
 
-void runTest(NodeHandle &nh) {
-
-    ROS_INFO("Waiting for initial state...");
-    sensor_msgs::JointStateConstPtr state =  ros::topic::waitForMessage<sensor_msgs::JointState>("/simulation/right_arm/joint_control/get_state", ros::Duration(3.0));
-    if(!state) {
-        ROS_ERROR("Unable to retrieve initial joint state - shutting down...");
-        exit(EXIT_FAILURE);
+bool getState(vector<double> &state) {
+    ROS_DEBUG("Waiting for current state...");
+    sensor_msgs::JointStateConstPtr current_state =  ros::topic::waitForMessage<sensor_msgs::JointState>(JOINT_STATES_TOPIC, ros::Duration(3.0));
+    if(!current_state) {
+        ROS_WARN("Unable to retrieve initial joint state!");
+        state.clear();
+        for (int i = 0; i < 7; ++i) {
+            state.push_back(0.0);
+        }
+        return false;
     }
-    ROS_INFO("Initial state received.");
-
-    ors::KinematicWorld G("kuka.kvg");
-    arr traj = plan(state->position);
-    displayTrajectory(traj, 1, G, "planned trajectory", 0.01);
-    execute(nh, traj);
+    state = current_state->position;
+    ROS_DEBUG("Initial state received.");
+    return true;
 }
+
+void runTest(iis_komo::KomoInterface &ki) {
+
+    ROS_INFO("Starting test run...");
+
+    ROS_INFO("Planning for first target...");
+
+    vector<double> init;
+    for (int i = 0; i < 7; ++i) {
+        init.push_back(0.0);
+    }
+
+    arr traj;
+    if(ki.plan(init, 0.3, 0.3, 0.5, 1.5, 0.0, 0.0, traj)) {
+        ROS_INFO("Trajectory planning request successful - executing...");
+//        ki.execute(traj);
+        ROS_INFO("Trajectory execution complete.");
+    }
+
+    ROS_INFO("Planning for second target...");
+    if(ki.plan(init, -0.3, -0.3, 0.7, traj)) {
+        ROS_INFO("Trajectory planning request successful - executing...");
+//        ki.execute(traj);
+        ROS_INFO("Trajectory execution complete.");
+    }
+
+    ROS_INFO("Test run complete");
+
+}
+
+//void test2() {
+
+//    ors::KinematicWorld world("kuka.kvg");
+
+//    ors::Shape *target = world.getShapeByName("target");
+////    target->X.addRelativeTranslation(0.0, 0.0, 1.6);
+
+//    cout << "Target body.x.pos: " << target->body->X.pos << endl;
+//    cout << "Target rel.pos: " << target->rel.pos << endl;
+//    cout << "Target X.pos: " << target->X.pos << endl;
+//    world.watch(true);
+
+//    target->X.addRelativeTranslation(0.1, 0.1, -0.1);
+
+//    cout << "Target body.x.pos: " << target->body->X.pos << endl;
+//    cout << "Target rel.pos: " << target->rel.pos << endl;
+//    cout << "Target X.pos: " << target->X.pos << endl;
+
+//    world.watch(true);
+
+//    target->reset();
+//    target->X.addRelativeTranslation(0.0, 0.0, 0.4);
+
+//    cout << "Target body.x.pos: " << target->body->X.pos << endl;
+//    cout << "Target rel.pos: " << target->rel.pos << endl;
+//    cout << "Target X.pos: " << target->X.pos << endl;
+
+//    world.watch(true);
+
+//    target->X.pos = ors::Vector(0.0, 0.0, 1.3);
+
+//    cout << "Target body.x.pos: " << target->body->X.pos << endl;
+//    cout << "Target rel.pos: " << target->rel.pos << endl;
+//    cout << "Target X.pos: " << target->X.pos << endl;
+
+//    world.watch(true);
+
+//}
 
 int main(int argc,char** argv) {
     MT::initCmdLine(argc,argv);
@@ -89,8 +138,10 @@ int main(int argc,char** argv) {
     spinner.start();
 
     NodeHandle nh;
+    _pub_move = nh.advertise<std_msgs::Float64MultiArray>(JOINT_MOVE_TOPIC, 1, false);
 
-    runTest(nh);
+    iis_komo::KomoInterface ki("kuka.kvg");
+    runTest(ki);
 
     spinner.stop();
     return EXIT_SUCCESS;
